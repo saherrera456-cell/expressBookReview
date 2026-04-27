755af37280b9b1c6d96d5374224be0c8655b1f88
saherrera456-cell <saherrera456@gmail.com>
2026-04-27 09:30:11 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,40 +1,14 @@
-const express = require('express');
-let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
-const public_users = express.Router();
-
-
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const username = req.body.username;
+  const password = req.body.password;
 
-public_users.get('/',function (req, res) {
-  // Reemplaza la línea del mensaje con esto:
-  return res.status(200).send(JSON.stringify(books, null, 4));
+  if (username && password) {
+    if (!users.find((user) => user.username === username)) {
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "Customer successfully registered. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
-
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  return res.status(200).send(books[isbn]);
-});
-  
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  let filtered_books = Object.values(books).filter((book) => book.author === author);
-  return res.status(200).send(JSON.stringify(filtered_books, null, 4));
-});
-
-public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  let filtered_books = Object.values(books).filter((book) => book.title === title);
-  return res.status(200).send(JSON.stringify(filtered_books, null, 4));
-});
-
-public_users.get('/review/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  return res.status(200).send(books[isbn].reviews);
-});
-
-module.exports.general = public_users;
